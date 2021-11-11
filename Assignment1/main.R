23e4571733c1e907c6d03098a631a69d7d6a5988
Daniël <59023527+De-Pool@users.noreply.github.com>
2021-11-11 17:19:19 +0100

    3

--- Assignment1/main.R
+++ Assignment1/main.R
@@ -1,8 +1,10 @@
+install.packages("arsenal")
+library(arsenal)
 # Read data
 data <- read.table("./Assignment1/data_assignment1.csv", header = TRUE,
                    sep = ",")
+str(data)
 summary(data)
-colnames(data)
 
 # Opdracht 1
 data$default_option <- as.factor(data$default_option)
@@ -15,3 +17,7 @@ data_normal_sp0 <- data[data$default_option == "normal" & data$searchperiod == 0
 data_normal_sp1 <- data[data$default_option == "normal" & data$searchperiod == 1, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
 summary(data_normal_sp0)
 summary(data_normal_sp1)
+
+# Opdracht 3
+balancing_table <- tableby(default_option ~ female + age + partner + children + years_education + suminc_before_application, data=data)
+summary(balancing_table, text=TRUE)
