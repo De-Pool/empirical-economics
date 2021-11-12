769721b96af79d6592cb78aab1ff124e0e49b981
Daniel <d.c.Poolman@student.tudelft.nl>
2021-11-12 17:27:21 +0100

    dummy

--- Assignment1/main.R
+++ Assignment1/main.R
@@ -9,14 +9,14 @@ str(data)
 summary(data)
 
 # Opdracht 1
-data$default_option <- as.factor(data$default_option)
+data$default_dummy <- ifelse(data$default_option == "normal", 1, 0)
 # sp_never > 0, therefore not all caseworkers complied with the default option
-average_sp_never <- mean(data[data$default_option == "never", "searchperiod"])
-average_sp_normal <- mean(data[data$default_option == "normal", "searchperiod"])
+average_sp_never <- mean(data[data$defaultdummy == 0, "searchperiod"])
+average_sp_normal <- mean(data[data$defaultdummy == 1, "searchperiod"])
 
 # Opdracht 2
-data_normal_sp0 <- data[data$default_option == "normal" & data$searchperiod == 0, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
-data_normal_sp1 <- data[data$default_option == "normal" & data$searchperiod == 1, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
+data_normal_sp0 <- data[data$defaultdummy == 1 & data$searchperiod == 0, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
+data_normal_sp1 <- data[data$defaultdummy == 1 & data$searchperiod == 1, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
 summary(data_normal_sp0)
 summary(data_normal_sp1)
 
@@ -25,5 +25,5 @@ balancing_table <- tableby(default_option ~ female + age + partner + children + 
 summary(balancing_table, text=TRUE)
 
 # Opdracht 5
-ols_fs <- lm(totweeksbenefits26 ~ searchperiod,data=data)
+ols_fs <- lm(totweeksbenefits26 ~ searchperiod + female + age + partner + children + years_education + suminc_before_application,data=data)
 summary(ols_fs)
