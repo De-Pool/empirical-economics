0343b43cc5d4817973b9f06c374b35334ae826b4
Daniel <d.c.Poolman@student.tudelft.nl>
2021-11-15 17:21:20 +0100

    update

--- Assignment1/main.R
+++ Assignment1/main.R
@@ -2,8 +2,7 @@ install.packages("arsenal")
 install.packages("AER")
 library(arsenal)
 library(AER)
-# install.packages("ivreg")
-# library(ivreg)
+
 # Read data
 data <- read.table("./Assignment1/data_assignment1.csv", header = TRUE,
                    sep = ",")
@@ -12,15 +11,15 @@ summary(data)
 
 # Opdracht 1
 # default_dummy takes on 1 if default_option = normal, else it takes on a value of 0
-data$defaultdummy <- ifelse(data$default_option == "normal", 1, 0)
+data$default_dummy <- ifelse(data$default_option == "normal", 1, 0)
 # sp_never > 0, therefore not all caseworkers complied with the default option
-average_sp_never <- mean(data[data$defaultdummy == 0, "searchperiod"])
-average_sp_normal <- mean(data[data$defaultdummy == 1, "searchperiod"])
-cor.test(data$defaultdummy, data$searchperiod)
+average_sp_never <- mean(data[data$default_dummy == 0, "searchperiod"])
+average_sp_normal <- mean(data[data$default_dummy == 1, "searchperiod"])
+cor.test(data$default_dummy, data$searchperiod)
 
 # Opdracht 2
-data_normal_sp0 <- data[data$defaultdummy == 1 & data$searchperiod == 0, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
-data_normal_sp1 <- data[data$defaultdummy == 1 & data$searchperiod == 1, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
+data_normal_sp0 <- data[data$default_dummy == 1 & data$searchperiod == 0, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
+data_normal_sp1 <- data[data$default_dummy == 1 & data$searchperiod == 1, c("female", "age", "partner", "children", "years_education", "suminc_before_application")]
 summary(data_normal_sp0)
 summary(data_normal_sp1)
 
@@ -33,33 +32,35 @@ summary(balancing_table, text = TRUE)
 # Zi = default_option (instrumental variable for searchperiod)
 # X = searchperiod (we want to assess what effect having a search period has on Y -> benefits = a + B1*searchperiod + u)
 # We use 2SLS to estimate B1
-ols_12SLS <- lm(searchperiod ~ defaultdummy, data = data)
+ols_12SLS <- lm(searchperiod ~ default_dummy, data = data)
 summary(ols_12SLS)
 
 # Opdracht 7
+# 2nd stage using the fitted values for searchperiod from the first stage
 x_hat <- ols_12SLS$fitted.values
 ols_22SLS <- lm(totweeksbenefits26 ~ x_hat, data = data)
 summary(ols_22SLS)
 
-# Direct IV estimation using 2SLS, with all control variables
-iv_estimation_without_controls <- ivreg(totweeksbenefits26 ~ searchperiod | defaultdummy, data = data)
-summary(iv_estimation_without_controls)
+# Direct IV estimation using 2SLS, with and without control variables
+iv_est_controls <- ivreg(totweeksbenefits26 ~ searchperiod + female + age + partner + children + years_education + suminc_before_application |
+      default_dummy + female + age + partner + children + years_education + suminc_before_application, data = data)
+summary(iv_est_controls)
 
-iv_estimation_controls <- ivreg(totweeksbenefits26 ~ searchperiod + female + age + partner + children + years_education + suminc_before_application |
-      defaultdummy + female + age + partner + children + years_education + suminc_before_application, data = data)
-summary(iv_estimation_controls)
+iv_est_no_controls <- ivreg(totweeksbenefits26 ~ searchperiod | default_dummy, data = data)
+summary(iv_est_no_controls)
 
 # Opdracht 10
 # 4 components, E[Yi|Zi=1], E[Yi|Zi=0], E[Xi|Zi=1], E[Xi|Zi=0]
-E_Yi_Zi_1 <- mean(data[data$defaultdummy == 1, "totweeksbenefits26"])
-E_Yi_Zi_0 <- mean(data[data$defaultdummy == 0, "totweeksbenefits26"])
-E_Xi_Zi_1 <- mean(data[data$defaultdummy == 1, "searchperiod"])
-E_Xi_Zi_0 <- mean(data[data$defaultdummy == 0, "searchperiod"])
+E_Yi_Zi_1 <- mean(data[data$default_dummy == 1, "totweeksbenefits26"])
+E_Yi_Zi_0 <- mean(data[data$default_dummy == 0, "totweeksbenefits26"])
+E_Xi_Zi_1 <- mean(data[data$default_dummy == 1, "searchperiod"])
+E_Xi_Zi_0 <- mean(data[data$default_dummy == 0, "searchperiod"])
 B_wald <- (E_Yi_Zi_1 - E_Yi_Zi_0) / (E_Xi_Zi_1 - E_Xi_Zi_0)
 
 # Opdracht 12
-iv_est_week_job <- ivreg(totweekswage26 ~ searchperiod | defaultdummy, data=data)
+iv_est_week_job <- ivreg(totweekswage26 ~ searchperiod | default_dummy, data=data)
 summary(iv_est_week_job)
-iv_est_wage <- ivreg(totincome26 ~ searchperiod | defaultdummy, data=data)
+
+iv_est_wage <- ivreg(totincome26 ~ searchperiod | default_dummy, data=data)
 summary(iv_est_wage)
 
